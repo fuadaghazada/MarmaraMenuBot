f115de8fcea912627cc6c833c9727a6e9555dc4e
Fuad Aghazada <fuad.aghazada98@gmail.com>
2019-02-21 23:35:18 +0300

    --

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from scraper import fetch_todays_menu, process_meal
     Bot function
 '''
 def bot_func(text = '', chat_id = ''):
-    if text == '/start':
-        sendMessage("Hello, I am Marmara Yemek Bot!\n You can: \n\t/fix - For having today's fix menu \n\t/alt - For having alternative menu \n\n\nBon Apetite!", chat_id)
+    if text == '/start' or text == '/help':
+        sendMessage("Hello, I am Marmara Menu Bot!\n You can: \n\t/fix - For having today's fix menu \n\t/alt - For having today's alternative menu \n\n\nBon Apetite!", chat_id)
     elif text == '/fix':
         meal = fetch_todays_menu()['fix']
         sendMessage('*Fix Menu*\n___Lunch___\n\n' + meal['lunch'], chat_id)
@@ -15,7 +15,7 @@ def bot_func(text = '', chat_id = ''):
         meal = fetch_todays_menu()['alt']
         sendMessage('*Alternative Menu*\n\n' + meal['lunch'], chat_id)
     else:
-        sendMessage('Unknown Command!', chat_id)
+        sendMessage('I dont understand you :(\n', chat_id)
 
 if __name__ == "__main__":
     update_bot(bot_func)
